43b85678a0dacf604f26bd154d4ff772edcf9ebb
Dan Tao <daniel.tao@gmail.com>
2013-02-17 13:25:43 -0800

    renamed Chart#label -> Chart#title

--- app/models/chart.rb
+++ app/models/chart.rb
@@ -2,9 +2,9 @@ class Chart
   include DataMapper::Resource
 
   property :id,          Serial
-  property :token,       String
+  property :token,       String, :unique_index => true
   property :chart_type,  String
-  property :label,       String
+  property :title,       String
   property :description, Text
   property :data,        Text
 end
