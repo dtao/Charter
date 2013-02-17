76a2983023cecf3f3c62882ba04fdfb4a049f816
Dan Tao <daniel.tao@gmail.com>
2013-02-17 13:25:22 -0800

    added title field

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,28 @@
 $(document).ready(function() {
   var chart = document.getElementById("rendered-chart");
+  var $title = $("input.title");
 
-  $("input", "#data-table").change(function() {
+  function renderChart() {
     HighTables.LineChart.renderTo(chart);
+  }
+
+  $("input", "#data-table").change(renderChart);
+
+  $("input.title").change(function() {
+    if ($.trim($title.val()) === "") {
+      $title.addClass("empty");
+    } else {
+      $title.removeClass("empty");
+    }
+
+    renderChart();
   });
+
+  window.getChartOptions = function() {
+    return {
+      title: {
+        text: $title.val()
+      }
+    };
+  };
 });
